405e70d524c57484eb2be530dd973ef4858806e4
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-03 09:30:17 -0700

    JS: connect input with card display

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // grab DOM elements
+const topTextInput = document.getElementById('top-text-input');
+const topTextDisplay = document.getElementById('top-text');
+
+// Constants
+// Grab values populated by html load so we don't duplicate definitions.
+const topTextDefault = topTextDisplay.textContent;
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+topTextInput.addEventListener('input', () => {
+    if (topTextInput.value) {
+        topTextDisplay.textContent = topTextInput.value;
+        topTextDisplay.classList.remove('placeholder');
+    } else {
+        topTextDisplay.textContent = topTextDefault;
+        topTextDisplay.classList.add('placeholder');
+    }
+});
